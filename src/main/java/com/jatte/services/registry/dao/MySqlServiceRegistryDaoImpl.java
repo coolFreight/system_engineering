2a99c396fb8f81cfd0870ebb2a58ce91ffdfc112
jovaughnlockridge1 <jova626@gmail.com>
2019-07-14 02:11:41 -0400

    adding HealthChecker implementation, new dao method to get all connected, thread pool to perform health check process

--- src/main/java/com/jatte/services/registry/dao/MySqlServiceRegistryDaoImpl.java
+++ src/main/java/com/jatte/services/registry/dao/MySqlServiceRegistryDaoImpl.java
@@ -31,8 +31,8 @@ public class MySqlServiceRegistryDaoImpl implements ServiceRegistryDao {
 
     static {
         ds.setUrl("jdbc:mysql://localhost:3306/");
-        ds.setUsername("root");
-        ds.setPassword("rainbow14");
+        ds.setUsername("");
+        ds.setPassword("");
         ds.setMinIdle(5);
         ds.setMaxIdle(10);
         ds.setMaxOpenPreparedStatements(100);
